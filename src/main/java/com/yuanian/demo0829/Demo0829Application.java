ecb076ff9d2473017092732a097c00aa6cc20b30
caibin2580 <caibin2580@163.com>
2019-08-29 17:30:30 +0800

    test0829

---
+++ src/main/java/com/yuanian/demo0829/Demo0829Application.java
@@ -0,0 +1,13 @@
+package com.yuanian.demo0829;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo0829Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo0829Application.class, args);
+	}
+
+}
